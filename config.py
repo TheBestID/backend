239f69fd6229821caa9f1e3d11d666c3f1100a45
A. Sergey <agibalov.sd@phystech.edu>
2022-10-24 16:00:47 +0300

    added table

---
+++ config.py
@@ -0,0 +1,9 @@
+# host = "localhost"
+# username = "postgres"
+# database = "postgres"
+# password = "12345"
+
+host = "babar.db.elephantsql.com"
+username = "ooldqkhh"
+database = "ooldqkhh"
+password = "6VST_qlsQDeuRGVzXkS1hsrgFhCX9MFz"
