53bd797e2e47c3f32310f2fc61ba3db36f6bb400
A. Sergey <agibalov.sd@phystech.edu>
2022-10-30 14:26:42 +0300

    cors open

--- smartcontracts/conn_to_sol.py
+++ smartcontracts/conn_to_sol.py
@@ -4,20 +4,18 @@ import time
 
 
 def send_data(smth: dict):
-    pass
+    w3 = Web3(Web3.HTTPProvider("https://goerli.infura.io/v3/bbd5ce33856f4a188df9a144746934e4"))
+    myContract = w3.eth.contract(address="0x61Cd0c3044F291A2A7fe08596D36Efd799cb7092", abi=ABI)
+    a = myContract.functions.store(12).transact()
+    print(a)
     return '1'
 
 
 def get_data(smth):
-    start = time.time()
     w3 = Web3(Web3.HTTPProvider("https://goerli.infura.io/v3/bbd5ce33856f4a188df9a144746934e4"))
-    # print(w3.isConnected())
-
     myContract = w3.eth.contract(address="0x61Cd0c3044F291A2A7fe08596D36Efd799cb7092", abi=ABI)
-    print(myContract.functions.retrieve().call())
-    print(time.time() - start)
-    pass
-    return {'Тестовые': "Данные"}
+    a = myContract.functions.retrieve()
+    return {'Тестовые': a}
 
 
-# get_data(1)
+# send_data(1)
