2c8ad90a36e1445d4971a0bbd0edb1ae47a81aad
maxxx <maxxxi_boyk@mail.ru>
2022-11-13 16:43:19 +0300

    vac

--- database/vacancy.py
+++ database/vacancy.py
@@ -131,7 +131,7 @@ async def get_vacancy(conn: Union[Connection, Pool], id: int):
         FROM vacancy WHERE id = $1;
         """, id)).get('ipfs_cid')
     data = getFromIpfs(cid)
-    return json({'owner_uuid': data.get('attributes')[1].get('owner_uuid'), 'price': data.get('attributes')[3].get('price'), 'category': data.get('attributes')[2].get('category'), 'info': data.get('description') })
+    return json({'owner_uuid': data.get('attributes')[1].get('owner_uuid'), 'price': data.get('attributes')[3].get('price'), 'category': data.get('attributes')[2].get('category'), 'info': data.get('description'), 'id': id })
 
 
 #change create_dump: add ach_uuid!!!
