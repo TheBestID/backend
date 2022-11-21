ecbe369b78f4bdaab36512047b53e0f7a2e06f42
A. Sergey <agibalov.sd@phystech.edu>
2022-11-21 12:12:32 +0300

    tried to connect NEAR

--- smartcontracts/conn_to_near.py
+++ smartcontracts/conn_to_near.py
@@ -1,16 +1,17 @@
 import near_api
 from web3 import Web3
 
-contract_id = "contract.testnet"
-signer_id = "signer.testnet"
-signer_key = "ed25519:SXDVLyDiLUgnxxujbZdS9reypb9TaYG7LgTLA2Aq3yFoX1YNd76CEhF2D1yKmAcFGbFF46o5mvGHx5N7BqMUjzf"
+contract_id = "sbt.soul_dev.testnet"
+signer_id = "soul_dev.testnet"
+signer_key = "ed25519:3gmjx9r37mpY5kPbyG3WpsrpjNpcWPdDxdWpXPAaDYUsRpYmk19w93Fe8T7p2Rirw71hdaMPqQyjfimSpcX9QCaK"
 args = {"counter": 1, "action": "increase"}
 
 near_provider = near_api.providers.JsonProvider("https://rpc.testnet.near.org")
 key_pair = near_api.signer.KeyPair(signer_key)
 signer = near_api.signer.Signer(signer_id, key_pair)
-account = near_api.account.Account(near_provider, signer, '1111')
+account = near_api.account.Account(near_provider, signer, signer_id)
+print(near_api.account.Account.access_key.setter(('nonce', 1)))
 
-out = account.function_call(contract_id, "counter_set", args)
+out = account.function_call(contract_id, "get_num", args)
 
-print(out)
+# print(out)
