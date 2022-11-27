1afa9b111616aaec86f1477afb1eef591c43bc43
Sergey <help@wherby.ru>
2022-11-27 15:21:59 +0300

    fixed admins

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,14 +1,16 @@
+from uuid import uuid4
+
 from sanic import Blueprint
 from sanic.response import Request, json, empty
-from uuid import uuid4
+from sanic_ext import openapi
 
 from database.achievements import create_table_achievements, get_table_achievements
 from database.achievements_request import get_table_ach_request, create_table_ach_request
 from database.userinfo import create_table_userinfo, get_table_userinfo, add_user_to_table
 from database.users import create_table_users, get_table_users
+from database.vacancy_request import get_table_vac_request, create_table_vac_request
 from database.verify import create_table_verify, get_table_verify
 from openapi.user import UserCheck
-from sanic_ext import openapi
 
 admin = Blueprint("admin", url_prefix="/admin")
 
@@ -62,6 +64,19 @@ async def get_bd_achievements(request: Request):
 async def clear_bd_achievements(request: Request):
     async with request.app.config.get('POOL').acquire() as conn:
         await create_table_achievements(conn, True)
+        return empty()
+
+
+@admin.get("/get_bd_vac_request")
+async def get_bd_ach_request(request: Request):
+    async with request.app.config.get('POOL').acquire() as conn:
+        return json(list(map(dict, await get_table_vac_request(conn))))
+
+
+@admin.get("/clear_bd_vac_request")
+async def clear_bd_ach_request(request: Request):
+    async with request.app.config.get('POOL').acquire() as conn:
+        await create_table_vac_request(conn, True)
         return empty()
 
 
